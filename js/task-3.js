f1fa86570e3cd42cdca8595fb6b645c132e6c2e2
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2024-12-15 19:54:03 +0200

    add markup header

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function filterArray(numbers, value) {
+    const filteredNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] > value) {
+        filteredNumbers.push(numbers[i]);
+      }
+    }
+    return filteredNumbers;
+  }
+  
