c5acbd2eb57d68bf0355a448ec536f3366fef5e0
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2024-12-16 13:36:43 +0200

    add markup header

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,14 @@
 function makeArray(firstArray, secondArray, maxLength) {
-    const combinedArray = firstArray.concat(secondArray);
-    if (combinedArray.length > maxLength) {
+  const combinedArray = firstArray.concat(secondArray);
+  if (combinedArray.length > maxLength) {
       return combinedArray.slice(0, maxLength);
-    }
-  
-    return combinedArray;
   }
+  return combinedArray;
+}
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 5)); // ["Mango", "Poly", "Ajax", "Chelsea"]
+console.log(makeArray([], ["Ajax", "Chelsea"], 2)); // ["Ajax", "Chelsea"]
+console.log(makeArray(["Mango"], [], 1)); // ["Mango"]
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Ajax", "Chelsea"]
